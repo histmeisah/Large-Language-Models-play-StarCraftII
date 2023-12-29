5c790a683e3d030beea36fad71af886845d1668a
ma wei yu <740988193@qq.com>
2023-12-29 19:09:39 +0800

    Update API_caller.py

--- LLM/API_caller.py
+++ LLM/API_caller.py
@@ -1,8 +1,8 @@
 import openai
 import os
 
-openai.api_key = "sk-8p38chfjXbbL1RT943B051229a224a8cBdE1B53b5e2c04E2"
-openai.api_base = "https://api.ai-yyds.com/v1"
+openai.api_key = ""
+openai.api_base = ""
 
 
 # os.environ["HTTP_PROXY"] = "http://127.0.0.1:7890"
